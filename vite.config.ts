07a42d43c761c32b2d49ce46db4bc11972c7be1a
HenriqueBaytek <henriquebaytek@gmail.com>
2025-11-06 15:54:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Substitua 'NOME_DO_SEU_REPOSITORIO' pelo nome exato do seu repositório no GitHub
 const REPO_NAME = 'Leitor-de-Pedidos'; 
 
 export default defineConfig({
   plugins: [react()],
+  
+  root: REPO_NAME,
+
   base: `/${REPO_NAME}/`,
-  // Esta seção é a chave!
-  // Ela diz ao Vite para substituir a string 'process.env.API_KEY'
-  // pelo valor da variável de ambiente VITE_GEMINI_API_KEY durante o build.
+
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY)
   }
